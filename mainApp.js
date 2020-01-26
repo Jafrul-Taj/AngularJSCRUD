9575998f44dfb22845f9949d9d8501ff0315f4a4
Jafrul Hasan Taj <jfhasan210@gmail.com>
2020-01-26 19:14:02 +0600

    Initial Commit

---
+++ mainApp.js
@@ -0,0 +1,15 @@
+var mainApp = angular.module("mainApp", []);
+
+mainApp.controller("myController", function($scope) {
+
+    $scope.users = [
+        { userName: "Fahim", fullName: "Fahim Arshad", email: "fahim@gmail.com" },
+        { userName: "Monir", fullName: "Monir-uz-Zaman", email: "monir@gmail.com" },
+        { userName: "Fayed", fullName: "Nur-Al-Fayed", email: "fayed@gmail.com" }
+    ];
+    $scope.newUser = {};
+    $scope.reset = function() {
+        console.log($scope.newUser);
+    }
+
+});
